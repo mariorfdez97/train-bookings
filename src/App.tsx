d2110cb440a29933c60c0d150df1023483627583
mariorfdez97 <UO296136@uniovi.es>
2026-07-08 10:28:48 +0200

    Inicializar ejercicio

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Reserva de Trenes</h1>
+      <p>Encuentra y reserva tu próximo viaje.</p>
+    </div>
+  )
+}
+
+export default App
